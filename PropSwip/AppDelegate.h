b8530c2008db7562d0f60be7c0a95f6bda0dda96
Kinshuk Kar <kinshuk.kar@gmail.com>
2016-04-02 17:58:27 +0530

    Initial Commit

---
+++ PropSwip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PropSwip
+//
+//  Created by Kinshuk  Kar on 4/2/16.
+//  Copyright © 2016 Kinshuk Kar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PropSwip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PropSwip
+//
+//  Created by Kinshuk  Kar on 4/2/16.
+//  Copyright © 2016 Kinshuk Kar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
